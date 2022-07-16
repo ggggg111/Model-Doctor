Fixed high-confidence samples path when extracting model gradients

src/diagnosing.py
@@ -68,9 +68,9 @@ args = parser.parse_args()
 def main():
     high_confidence_samples_path = args.high_confidence_samples_path
     dataset = args.dataset
-    high_confidence_samples_path = os.path.join(high_confidence_samples_path, dataset)
     device = torch.device(args.device)
     model_name = args.model_name
+    high_confidence_samples_path = os.path.join(high_confidence_samples_path, dataset, model_name)
     checkpoints_path = os.path.join(args.checkpoints_path, dataset)
     checkpoint_file = args.checkpoint_file
     checkpoint_path = os.path.join(checkpoints_path, checkpoint_file)